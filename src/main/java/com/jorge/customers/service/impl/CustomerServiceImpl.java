c23261055a82458b5510d7c76155a2db6f57e94f
Jorge <joseph_adagio_7700@hotmail.com>
2025-04-03 19:59:21 -0500

    Add basic logging

--- src/main/java/com/jorge/customers/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/jorge/customers/service/impl/CustomerServiceImpl.java
@@ -8,6 +8,7 @@ import com.jorge.customers.model.CustomerType;
 import com.jorge.customers.repository.CustomerRepository;
 import com.jorge.customers.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,18 +17,21 @@ import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
 
     @Override
     public Flux<CustomerResponse> getAllCustomers() {
+        log.info("Fetching all customers.");
         return customerRepository.findAll()
                 .map(customerMapper::mapToCustomerResponse);
     }
 
     @Override
     public Mono<CustomerResponse> getCustomerByDni(String dni) {
+        log.info("Fetching customer by DNI: {}", dni);
         return customerRepository.findByDni(dni)
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Cliente con dni: " + dni + " no encontrado")))
@@ -36,12 +40,14 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Mono<CustomerResponse> createCustomer(CustomerRequest customerRequest) {
+        log.info("Creating customer: {}", customerRequest);
         return customerRepository.save(customerMapper.mapToCustomer(customerRequest))
                 .map(customerMapper::mapToCustomerResponse);
     }
 
     @Override
     public Mono<CustomerResponse> updateCustomerByDni(String dni, CustomerRequest customerRequest) {
+        log.info("Updating customer with DNI: {} with data: {}", dni, customerRequest);
         return customerRepository.findByDni(dni)
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Cliente con dni: " + dni + " no encontrado")))
@@ -52,10 +58,12 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Mono<Void> deleteCustomerByDni(String dni) {
+        log.info("Deleting customer with DNI: {}", dni);
         return customerRepository.deleteByDni((dni));
     }
 
     public Customer updateCustomerFromRequest(Customer existingCustomer, CustomerRequest customerRequest) {
+        log.debug("Updating existing customer: {} with request: {}", existingCustomer, customerRequest);
         existingCustomer.setCustomerType(CustomerType.valueOf(customerRequest.getCustomerType().name()));
         existingCustomer.setEmail(customerRequest.getEmail());
         existingCustomer.setPhoneNumber(customerRequest.getPhoneNumber());
@@ -63,6 +71,7 @@ public class CustomerServiceImpl implements CustomerService {
         existingCustomer.setDni(customerRequest.getDni());
         existingCustomer.setFirstName(customerRequest.getFirstName());
         existingCustomer.setLastName(customerRequest.getLastName());
+        log.debug("Updated customer: {}", existingCustomer);
         return existingCustomer;
     }
 }
